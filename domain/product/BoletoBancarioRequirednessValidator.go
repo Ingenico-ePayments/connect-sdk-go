a7a7bc8bc8991d0226efdcc050f7558d620620d1
Ingenico ePayments <jenkins@isaac.nl>
2017-03-23 09:15:29 +0100

    Release 1.3.0.

---
+++ domain/product/BoletoBancarioRequirednessValidator.go
@@ -0,0 +1,15 @@
+// This class was auto-generated from the API references found at
+// https://developer.globalcollect.com/documentation/api/server/
+
+package product
+
+// BoletoBancarioRequirednessValidator represents class BoletoBancarioRequirednessValidator
+// Documentation can be found at https://developer.globalcollect.com/documentation/api/server/#schema_BoletoBancarioRequirednessValidator
+type BoletoBancarioRequirednessValidator struct {
+	FiscalNumberLength *int32 `json:"fiscalNumberLength,omitempty"`
+}
+
+// NewBoletoBancarioRequirednessValidator constructs a new BoletoBancarioRequirednessValidator
+func NewBoletoBancarioRequirednessValidator() *BoletoBancarioRequirednessValidator {
+	return &BoletoBancarioRequirednessValidator{}
+}
