5646d9987654c2b16fe4ed02d3367293215ab9d2
Ingenico ePayments <jenkins@isaac.nl>
2017-11-25 14:08:17 +0100

    Release 1.13.0.

---
+++ domain/payment/RefundEInvoiceMethodSpecificOutput.go
@@ -0,0 +1,15 @@
+// This class was auto-generated from the API references found at
+// https://epayments-api.developer-ingenico.com/s2sapi/v1/
+
+package payment
+
+// RefundEInvoiceMethodSpecificOutput represents class RefundEInvoiceMethodSpecificOutput
+type RefundEInvoiceMethodSpecificOutput struct {
+	TotalAmountPaid     *int64 `json:"totalAmountPaid,omitempty"`
+	TotalAmountRefunded *int64 `json:"totalAmountRefunded,omitempty"`
+}
+
+// NewRefundEInvoiceMethodSpecificOutput constructs a new RefundEInvoiceMethodSpecificOutput
+func NewRefundEInvoiceMethodSpecificOutput() *RefundEInvoiceMethodSpecificOutput {
+	return &RefundEInvoiceMethodSpecificOutput{}
+}
--- domain/payment/RefundOutput.go
+++ domain/payment/RefundOutput.go
@@ -7,14 +7,15 @@ import "github.com/Ingenico-ePayments/connect-sdk-go/domain/definitions"
 
 // RefundOutput represents class RefundOutput
 type RefundOutput struct {
-	AmountOfMoney                     *definitions.AmountOfMoney         `json:"amountOfMoney,omitempty"`
-	AmountPaid                        *int64                             `json:"amountPaid,omitempty"`
-	BankRefundMethodSpecificOutput    *RefundBankMethodSpecificOutput    `json:"bankRefundMethodSpecificOutput,omitempty"`
-	CardRefundMethodSpecificOutput    *RefundCardMethodSpecificOutput    `json:"cardRefundMethodSpecificOutput,omitempty"`
-	EWalletRefundMethodSpecificOutput *RefundEWalletMethodSpecificOutput `json:"eWalletRefundMethodSpecificOutput,omitempty"`
-	MobileRefundMethodSpecificOutput  *RefundMobileMethodSpecificOutput  `json:"mobileRefundMethodSpecificOutput,omitempty"`
-	PaymentMethod                     *string                            `json:"paymentMethod,omitempty"`
-	References                        *References                        `json:"references,omitempty"`
+	AmountOfMoney                      *definitions.AmountOfMoney          `json:"amountOfMoney,omitempty"`
+	AmountPaid                         *int64                              `json:"amountPaid,omitempty"`
+	BankRefundMethodSpecificOutput     *RefundBankMethodSpecificOutput     `json:"bankRefundMethodSpecificOutput,omitempty"`
+	CardRefundMethodSpecificOutput     *RefundCardMethodSpecificOutput     `json:"cardRefundMethodSpecificOutput,omitempty"`
+	EInvoiceRefundMethodSpecificOutput *RefundEInvoiceMethodSpecificOutput `json:"eInvoiceRefundMethodSpecificOutput,omitempty"`
+	EWalletRefundMethodSpecificOutput  *RefundEWalletMethodSpecificOutput  `json:"eWalletRefundMethodSpecificOutput,omitempty"`
+	MobileRefundMethodSpecificOutput   *RefundMobileMethodSpecificOutput   `json:"mobileRefundMethodSpecificOutput,omitempty"`
+	PaymentMethod                      *string                             `json:"paymentMethod,omitempty"`
+	References                         *References                         `json:"references,omitempty"`
 }
 
 // NewRefundOutput constructs a new RefundOutput
