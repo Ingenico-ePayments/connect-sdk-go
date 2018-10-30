9d80c1faa50e7cabb25f1e790d6aaf35e7a3195f
Ingenico ePayments <jenkins@isaac.nl>
2018-10-30 14:41:56 +0100

    Release 1.27.0.

---
+++ domain/payment/Product3201SpecificOutput.go
@@ -0,0 +1,16 @@
+// This class was auto-generated from the API references found at
+// https://epayments-api.developer-ingenico.com/s2sapi/v1/
+
+package payment
+
+import "github.com/Ingenico-ePayments/connect-sdk-go/domain/definitions"
+
+// Product3201SpecificOutput represents class PaymentProduct3201SpecificOutput
+type Product3201SpecificOutput struct {
+	Card *definitions.CardEssentials `json:"card,omitempty"`
+}
+
+// NewProduct3201SpecificOutput constructs a new Product3201SpecificOutput
+func NewProduct3201SpecificOutput() *Product3201SpecificOutput {
+	return &Product3201SpecificOutput{}
+}
--- domain/payment/RedirectPaymentMethodSpecificOutput.go
+++ domain/payment/RedirectPaymentMethodSpecificOutput.go
@@ -7,12 +7,13 @@ import "github.com/Ingenico-ePayments/connect-sdk-go/domain/definitions"
 
 // RedirectPaymentMethodSpecificOutput represents class RedirectPaymentMethodSpecificOutput
 type RedirectPaymentMethodSpecificOutput struct {
-	BankAccountIban                 *definitions.BankAccountIban `json:"bankAccountIban,omitempty"`
-	FraudResults                    *definitions.FraudResults    `json:"fraudResults,omitempty"`
-	PaymentProduct836SpecificOutput *Product836SpecificOutput    `json:"paymentProduct836SpecificOutput,omitempty"`
-	PaymentProduct840SpecificOutput *Product840SpecificOutput    `json:"paymentProduct840SpecificOutput,omitempty"`
-	PaymentProductID                *int32                       `json:"paymentProductId,omitempty"`
-	Token                           *string                      `json:"token,omitempty"`
+	BankAccountIban                  *definitions.BankAccountIban `json:"bankAccountIban,omitempty"`
+	FraudResults                     *definitions.FraudResults    `json:"fraudResults,omitempty"`
+	PaymentProduct3201SpecificOutput *Product3201SpecificOutput   `json:"paymentProduct3201SpecificOutput,omitempty"`
+	PaymentProduct836SpecificOutput  *Product836SpecificOutput    `json:"paymentProduct836SpecificOutput,omitempty"`
+	PaymentProduct840SpecificOutput  *Product840SpecificOutput    `json:"paymentProduct840SpecificOutput,omitempty"`
+	PaymentProductID                 *int32                       `json:"paymentProductId,omitempty"`
+	Token                            *string                      `json:"token,omitempty"`
 }
 
 // NewRedirectPaymentMethodSpecificOutput constructs a new RedirectPaymentMethodSpecificOutput
